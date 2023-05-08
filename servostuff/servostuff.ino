b6b4390169f9d3ce1018db3e2e399fc4efc5aa5e
Abhay Parmar <113143064+aparm539@users.noreply.github.com>
2023-05-07 19:15:20 -0600

    Change mapping of BNO055 to servo angles. Remove unused code.

--- servostuff/servostuff.ino
+++ servostuff/servostuff.ino
@@ -4,111 +4,44 @@
 #include <utility/imumaths.h>
 #include <Servo.h>
 
-/* Returns the IMU data as both a euler angles and quaternions as the WebSerial
-   3D Model viewer at https://adafruit-3dmodel-viewer.glitch.me/ expects.
- 
-   This driver uses the Adafruit unified sensor library (Adafruit_Sensor),
-   which provides a common 'type' for sensor data and some helper functions.
-
-   To use this driver you will also need to download the Adafruit_Sensor
-   library and include it in your libraries folder.
-
-   You should also assign a unique ID to this sensor for use with
-   the Adafruit Sensor API so that you can identify this particular
-   sensor in any data logs, etc.  To assign a unique ID, simply
-   provide an appropriate value in the constructor below (12345
-   is used by default in this example).
-
-   Connections
-   ===========
-   Connect SCL to analog 5
-   Connect SDA to analog 4
-   Connect VDD to 3.3-5V DC
-   Connect GROUND to common ground
-
-   History
-   =======
-   2020/JUN/01  - First release (Melissa LeBlanc-Williams)
+/* Author Sunny Parmar.
 */
 
 /* Set the delay between fresh samples */
 #define BNO055_SAMPLERATE_DELAY_MS (5)
-
-// Check I2C device address and correct line below (by default address is 0x29 or 0x28)
-//                                   id, address
 Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
-
-
-/**************************************************************************/
-/*
-    Arduino setup function (automatically called at startup)
-*/
-/**************************************************************************/
-  Servo rollServo;
-  Servo pitchServo;
-  Servo servo3;
-  int pitch; 
-  int roll;
-  int newPitch;
-  int newRoll;
-
+Servo rollServo;
+Servo pitchServo;
+Servo servo3;
+int pitch; 
+int roll;
+int newPitch;
+int newRoll;
 void setup(void)
 {
   Serial.begin(9600);
-  
-
   /* Initialise the sensor */
-  if(!bno.begin())
-  {
-    /* There was a problem detecting the BNO055 ... check your connections */
-    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
-    while(1);
-  }
-   
+  bno.begin(); 
   delay(1000);
-
   /* Use external crystal for better accuracy */
   bno.setExtCrystalUse(true);
    
   rollServo.attach(6);
   pitchServo.attach(5);
-  servo3.attach(3);
-
-
- 
+  servo3.attach(3); 
 }
 
-/**************************************************************************/
-/*
-    Arduino loop function, called once 'setup' is complete (your own code
-    should go here)
-*/
-/**************************************************************************/
 void loop(void)
 {
   /* Get a new sensor event */
   sensors_event_t event;
   bno.getEvent(&event);
 
-  /* Board layout:
-         +----------+
-         |         *| RST   PITCH  ROLL  HEADING
-     ADR |*        *| SCL
-     INT |*        *| SDA     ^            /->
-     PS1 |*        *| GND     |            |
-     PS0 |*        *| 3VO     Y    Z-->    \-X
-         |         *| VIN
-         +----------+
-  */
-
-
-
-
   pitch = (int)event.orientation.z;
   roll = (int)event.orientation.y;
 
-  newPitch = map(pitch,-180,180,180,0);
-  newRoll=map(roll,-180,180,0,180);
+  newPitch = map(pitch,-360,360,180,0);
+  newRoll=map(roll,-360,260,180,0);
 
   pitchServo.write(newPitch);
   rollServo.write(newRoll);
