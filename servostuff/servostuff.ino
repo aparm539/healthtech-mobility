56747986123cdf2c7fe9868cffc1afa73c2dd161
Abhay Parmar <113143064+aparm539@users.noreply.github.com>
2023-05-07 20:03:14 -0600

    Create a new PID algorithm for servo control.
    Use a PID control algorithm to adjust servo angles and maintain upright position. Previous attempts at servo control either resulted in oscillations or unstable behaviour. ki and kd set to zero for simplify the algorithm for initial testing but may cause unintended behaviour. This method no longer uses mapping which caused issues at the end of the range.

--- servostuff/servostuff.ino
+++ servostuff/servostuff.ino
@@ -1,51 +1,73 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
-#include <utility/imumaths.h>
 #include <Servo.h>
 
 /* Author Sunny Parmar.
 */
 
-/* Set the delay between fresh samples */
-#define BNO055_SAMPLERATE_DELAY_MS (5)
 Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
-Servo rollServo;
-Servo pitchServo;
-Servo servo3;
-int pitch; 
-int roll;
-int newPitch;
-int newRoll;
-void setup(void)
+Servo rollServo, pitchServo, servo3;
+
+int pitch, roll, newPitch, newRoll;
+double kp = 1.0; 
+double ki = 0.0; // temporarily set to zero 
+double kd = 0.0; // temporarily set to zero 
+double dt = 0.01; 
+
+double target_roll = 0.0; 
+double target_pitch = 0.0; 
+
+double last_error_roll = 0.0; 
+double last_error_pitch = 0.0; 
+double integral_error_roll= 0.0;
+double integral_erro_pitch=0.0;
+
+void setup()
 {
-  Serial.begin(9600);
-  /* Initialise the sensor */
-  bno.begin(); 
-  delay(1000);
-  /* Use external crystal for better accuracy */
-  bno.setExtCrystalUse(true);
-   
+  // Initialize the BNO055 sensor
+  bno.begin();
+  
+  // Initialize the servos
   rollServo.attach(6);
   pitchServo.attach(5);
-  servo3.attach(3); 
 }
 
-void loop(void)
+void loop()
 {
-  /* Get a new sensor event */
-  sensors_event_t event;
-  bno.getEvent(&event);
+  // Read orientation data 
+  imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+  
+  double roll = euler.x();    
+  double pitch = euler.y();   
+  
+  // Calculate the errors for roll and pitch
+  double error_roll = target_roll - roll;
+  double error_pitch = target_pitch - pitch;
+  
+  // Calculate the derivative errors for roll and pitch
+  double derivative_error_roll = (error_roll - last_error_roll) / dt;
+  double derivative_error_pitch = (error_pitch - last_error_pitch) / dt;
+  
+  // Update the last errors for roll and pitch
+  last_error_roll = error_roll;
+  last_error_pitch = error_pitch;
+  
+  // Calculate the integral errors for roll and pitch
+  integral_error_roll += error_roll * dt;
+  integral_error_pitch += error_pitch * dt;
+  
+  // Calculate the PID output for each servo
+  double output1 = kp * error_roll + ki * integral_error_roll + kd * derivative_error_roll;
+  double output2 = kp * error_pitch + ki * integral_error_pitch + kd * derivative_error_pitch;
+ 
+  
+  // Adjust the servo angles based on the PID output
+  rollServo.write(90 + output1); // 90 is a placeholder for the starting/ ideal position
+  pitchServo.write(90 + output2);
 
-  pitch = (int)event.orientation.z;
-  roll = (int)event.orientation.y;
-
-  newPitch = map(pitch,-360,360,180,0);
-  newRoll=map(roll,-360,260,180,0);
-
-  pitchServo.write(newPitch);
-  rollServo.write(newRoll);
-
-
-  delay(BNO055_SAMPLERATE_DELAY_MS);
+  
+  // Wait for the sample rate
+  delay(dt * 1000);
 }
+
