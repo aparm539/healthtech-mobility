4946f4826165ba9e1f47fd2ff17a067694f9b95a
Abhay Parmar <113143064+aparm539@users.noreply.github.com>
2023-05-21 14:46:15 -0600

    Change algorithm to match sampling rate of BNO055.
    Previous version utilized the delay method to match the sampling rate.
    While in the delay method Servos kept moving causing jitter both while
    moving the product and at rest. New method removes all jitter in motion
    and significantly reduces jitter at rest.

--- servostuff/servostuff.ino
+++ servostuff/servostuff.ino
@@ -14,6 +14,8 @@ double kp = 1.0;
 double ki = 0.0; // temporarily set to zero 
 double kd = 0.0; // temporarily set to zero 
 double dt = 0.01; 
+unsigned long currentTime;
+unsigned long readRate =10.00;
 
 double target_roll = 0.0; 
 double target_pitch = 0.0; 
@@ -21,7 +23,7 @@ double target_pitch = 0.0;
 double last_error_roll = 0.0; 
 double last_error_pitch = 0.0; 
 double integral_error_roll= 0.0;
-double integral_erro_pitch=0.0;
+double integral_error_pitch=0.0;
 
 void setup()
 {
@@ -35,39 +37,38 @@ void setup()
 
 void loop()
 {
-  // Read orientation data 
-  imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+  currentTime = millis();
+  if (currentTime % readRate){
+    // Read orientation data 
+    imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+    double roll = euler.z();    
+    double pitch = euler.y();   
+    
+    // Calculate the errors for roll and pitch
+    double error_roll = target_roll - roll;
+    double error_pitch = target_pitch - pitch;
+    
+    // Calculate the derivative errors for roll and pitch
+    double derivative_error_roll = (error_roll - last_error_roll) / dt;
+    double derivative_error_pitch = (error_pitch - last_error_pitch) / dt;
+    
+    // Update the last errors for roll and pitch
+    last_error_roll = error_roll;
+    last_error_pitch = error_pitch;
+    
+    // Calculate the integral errors for roll and pitch
+    integral_error_roll += error_roll * dt;
+    integral_error_pitch += error_pitch * dt;
+    
+    // Calculate the PID output for each servo
+    double output1 = -kp * error_roll + ki * integral_error_roll + kd * derivative_error_roll;
+    double output2 = kp * error_pitch + ki * integral_error_pitch + kd * derivative_error_pitch;
   
-  double roll = euler.x();    
-  double pitch = euler.y();   
-  
-  // Calculate the errors for roll and pitch
-  double error_roll = target_roll - roll;
-  double error_pitch = target_pitch - pitch;
-  
-  // Calculate the derivative errors for roll and pitch
-  double derivative_error_roll = (error_roll - last_error_roll) / dt;
-  double derivative_error_pitch = (error_pitch - last_error_pitch) / dt;
-  
-  // Update the last errors for roll and pitch
-  last_error_roll = error_roll;
-  last_error_pitch = error_pitch;
-  
-  // Calculate the integral errors for roll and pitch
-  integral_error_roll += error_roll * dt;
-  integral_error_pitch += error_pitch * dt;
-  
-  // Calculate the PID output for each servo
-  double output1 = kp * error_roll + ki * integral_error_roll + kd * derivative_error_roll;
-  double output2 = kp * error_pitch + ki * integral_error_pitch + kd * derivative_error_pitch;
- 
-  
-  // Adjust the servo angles based on the PID output
-  rollServo.write(90 + output1); // 90 is a placeholder for the starting/ ideal position
-  pitchServo.write(90 + output2);
+    
+    // Adjust the servo angles based on the PID output
+    rollServo.write(output1); 
+    pitchServo.write(90 + output2); // 90 is the current ideal position
+  }
 
-  
-  // Wait for the sample rate
-  delay(dt * 1000);
 }
 
